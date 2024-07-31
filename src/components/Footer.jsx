e582ebccb2684182dea5a750b112a853f604b261
leongweitong <weitong0724@gmail.com>
2024-08-01 00:05:44 +0800

    download react icon and sample page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BsHouse, BsList, BsBarChart, BsFlag } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='fixed bottom-0 inset-x-0 border-t border-secondary bg-white'>
+        <div className='container px-4 py-3'>
+            <div className='flex justify-around'>
+                <BsHouse className='text-3xl text-primary' />
+                <BsBarChart className='text-3xl' />
+                <BsFlag className='text-3xl' />
+                <BsList className='text-3xl' />
+            </div>
+        </div>
+    </footer>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgba(230, 56, 37, 255)',
+        secondary: 'rgba(225, 225, 225, 255)',
+        black: 'rgba(0, 0, 0, 255)',
+        white: 'rgba(255, 255, 255, 255)',
+      },
+    },
   },
   plugins: [],
 }
