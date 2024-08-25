e800a2c2f6d3195c41e4f5b731f00c5f1884ca30
leongweitong <weitong0724@gmail.com>
2024-08-26 00:32:27 +0800

    update analysis page

--- src/pages/AnalysisPage.jsx
+++ src/pages/AnalysisPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useTranslation } from "react-i18next";
-import { AreaChart, LineChart, Line, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, AreaChart, Area, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { useIndexedDB } from "react-indexed-db-hook";
 
 const AnalysisPage = () => {
@@ -53,39 +53,68 @@ const AnalysisPage = () => {
       return {
         date: item.date,
         speed: parseFloat(speed), // Convert to a float for chart display
+        totalTime: totalHours.toFixed(2),
       };
     });
   
     console.log("Processed Activity Data:", result);
   
     return result;
-  };  
+  }; 
 
   return (
     <div className='flex flex-col gap-4'>
       <div className="border-b border-secondary px-4 py-3">
         <div className='text-primary text-xl font-semibold'>{t("analysis")}</div>
       </div>
-
-      <div className='px-4'>
-        <div className='border border-primary rounded-xl p-4'>
-          <ResponsiveContainer width="100%" height={250}>
-              <AreaChart data={data} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
-                <defs>
-                  <linearGradient id="colorSpeed" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="5%" stopColor="#e63825f2" stopOpacity={0.8}/>
-                    <stop offset="95%" stopColor="#e63825f2" stopOpacity={0}/>
-                  </linearGradient>
-                </defs>
-                <CartesianGrid strokeDasharray="3 3" />
-                <Tooltip />
-                <Area type="monotone" dataKey="speed" stroke="#e63825f2" fillOpacity={1} fill="url(#colorSpeed)" />
-              </AreaChart>
-            </ResponsiveContainer>
-        </div>
-      </div>
+  
+      {data.length > 0 ? (
+        <>
+          <div className='px-4'>
+            <div className='p-4'>
+              <ResponsiveContainer width="100%" height={250}>
+                <BarChart data={data} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
+                  <defs>
+                    <linearGradient id="colorTime" x1="0" y1="0" x2="0" y2="1">
+                      <stop offset="5%" stopColor="#e63825f2" stopOpacity={0.8} />
+                      <stop offset="95%" stopColor="#e63825f2" stopOpacity={0} />
+                    </linearGradient>
+                  </defs>
+                  <Tooltip />
+                  <Legend verticalAlign="top" height={36} />
+                  <Bar 
+                    dataKey="totalTime" 
+                    fill="url(#colorTime)" 
+                    name={t("time-spent-running-hour")} 
+                  />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+  
+          <div className='px-4'>
+            <div className='p-4'>
+              <ResponsiveContainer width="100%" height={250}>
+                <AreaChart data={data} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
+                  <defs>
+                    <linearGradient id="colorSpeed" x1="0" y1="0" x2="0" y2="1">
+                      <stop offset="5%" stopColor="#e63825f2" stopOpacity={0.8}/>
+                      <stop offset="95%" stopColor="#e63825f2" stopOpacity={0}/>
+                    </linearGradient>
+                  </defs>
+                  <Tooltip />
+                  <Legend verticalAlign="top" height={36}/>
+                  <Area type="monotone" dataKey="speed" name={t("speed-analysis-hour")} stroke="#e63825f2" fillOpacity={1} fill="url(#colorSpeed)" />
+                </AreaChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className='mt-4 text-center text-gray-500'>{t("general.no-record")}</div>
+      )}
     </div>
-  )
+  );  
 }
 
 export default AnalysisPage;
